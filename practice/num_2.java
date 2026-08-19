f46de0d8c987843e72133599b2fab1b7a19a4d3d
HDZ <kp5861213@163.com>
2026-08-19 21:58:25 +0800

    百题计划_2

---
+++ practice/num_2.java
@@ -0,0 +1,25 @@
+package practice;
+
+import java.util.*;
+
+public class num_2 {
+    List<List<String>> result = new ArrayList<>();
+    Map<String, List<String>> strToListMap = new HashMap<>();
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+        for (String str : strs) {
+            char[] charList = str.toCharArray();
+            Arrays.sort(charList);
+            String strKey = new String(charList);
+            List<String> defaultList = strToListMap.getOrDefault(strKey, new ArrayList<>());
+            defaultList.add(str);
+            strToListMap.put(strKey,defaultList);
+        }
+        return strToListMap.values().stream().toList();
+    }
+
+    public static void main(String[] args) {
+        num_2 num2 = new num_2();
+        num2.groupAnagrams(new String[]{"eat", "tea", "tan", "ate", "nat", "bat"});
+    }
+}
